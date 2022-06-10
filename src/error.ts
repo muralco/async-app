726eacd55fa456604fbe00afe7811d08e9b486bf
Zach Brandt <zacharybrandt10@gmail.com>
2022-06-10 15:57:43 +0200

    Call super with the message passed in, if it exists

--- src/error.ts
+++ src/error.ts
@@ -4,7 +4,7 @@ export class CustomError extends Error {
   extra: unknown;
 
   constructor(statusCode: number, error?: string, extra?: unknown) {
-    super('CustomError');
+    super(error || 'CustomError');
     this.statusCode = statusCode;
     this.error = error;
     this.extra = extra

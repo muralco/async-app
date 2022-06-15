30788f2c17ad2eb170b1cdc092de4092cf5bfeb8
Nicola <npalavec@itba.edu.ar>
2022-06-15 18:41:39 -0300

    Add parameter to new algorithm to support new functionality

--- src/converters/order.ts
+++ src/converters/order.ts
@@ -1,16 +1,37 @@
 import {
   Converter,
   Entities,
+  isProviderMiddleware,
+  Middleware,
+  ProviderMiddleware,
 } from '../types';
 import { getOrderConverter } from './common';
 
 export const converterId = 'orderConverterV2';
 
-export default function orderConverter<
-  TEntities extends Entities,
-  TSchema
->(): Converter<TEntities, TSchema> {
-  const converter = getOrderConverter<TEntities, TSchema>();
+export interface OrderOptions {
+  noStableSort: boolean;
+}
+
+const providerOrderMethod = <TEntities extends Entities>(
+  providers: ProviderMiddleware<TEntities>[],
+  middlewares: Middleware<TEntities>[],
+) => {
+  // This will return all the providers in the order as they appear in the
+  // middlewares list
+  return middlewares.filter(
+    (m): m is ProviderMiddleware<TEntities> =>
+      isProviderMiddleware(m) && providers.includes(m),
+  );
+};
+
+export default function orderConverter<TEntities extends Entities, TSchema>(
+  opts: Partial<OrderOptions> = {},
+): Converter<TEntities, TSchema> {
+  const { noStableSort } = opts;
+  const converter = getOrderConverter<TEntities, TSchema>(
+    !noStableSort ? providerOrderMethod : undefined,
+  );
   converter.converterId = converterId;
   return converter;
 }

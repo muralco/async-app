509fb5c958f8ba066c56b6321a6690690ebcbcc8
Nicola Palavecino <nicolap@mural.co>
2023-08-10 20:18:04 -0300

    Replace initial monkeypatching with app provider pattern for instrumentation

--- src/create-app.ts
+++ src/create-app.ts
@@ -16,6 +16,10 @@ import {
   RequestHandler,
 } from './types';
 
+type AppProvider<TEntities extends Entities = Entities, TSchema = {}> = (
+  opts?: Opts<TEntities, TSchema>
+) => App<TEntities, TSchema>;
+
 const isPath = (arg: unknown) => (typeof arg === 'string' || isRegExp(arg));
 
 const patchMethod = <
@@ -67,8 +71,11 @@ const METHODS: Method[] = [
   'use',
 ];
 
-export const createApp = <TEntities extends Entities = Entities, TSchema = {}>(
-  opts?: Opts<TEntities, TSchema>,
+const asyncAppProvider: AppProvider = <
+  TEntities extends Entities = Entities,
+  TSchema = {}
+>(
+  opts?: Opts<TEntities, TSchema>
 ): App<TEntities, TSchema> => {
   const app = express() as App<TEntities, TSchema>;
   const converters = opts && opts.converters || [];
@@ -103,5 +110,16 @@ export const createApp = <TEntities extends Entities = Entities, TSchema = {}>(
 
   return app;
 };
+
+let appProvider: AppProvider<any, any> = asyncAppProvider;
+
+export const resetProvider = () => (appProvider = asyncAppProvider);
+
+export const setProvider = (provider: AppProvider<any, any>) =>
+  (appProvider = provider);
+
+export const createApp = <TEntities extends Entities = Entities, TSchema = {}>(
+  opts?: Opts<TEntities, TSchema>
+) => appProvider(opts);
 
 export default createApp;
